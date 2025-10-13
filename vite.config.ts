a246a49f5f77c738a80b7eb893b94ae0ed6db03d
Damian-Sonwa <madudamian25@gmail.com>
2025-10-13 04:21:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    allowedHosts:
-    ["tradiolife-nature-ai.onrender.com"]
-    port: 8080,
-  },
+server: {
+  allowedHosts: ['tradiolife-nature-ai.onrender.com'], // ← comma here
+  port: 8080,
+},
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
